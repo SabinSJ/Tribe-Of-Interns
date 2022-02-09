c0db7408cd9817b9f76834d390727357dc31681b
Florin-Sabin Sarca <39478000+SabinSJ@users.noreply.github.com>
2022-02-09 17:13:15 +0200

    Connect Express to MongoDb
    In this update, we are able to connect our backend to MongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
-const res = require('express/lib/response')
-
+const mongoose = require('mongoose')
 const app = express()
+const EmployeeRoute = require('./route/EmployeeRoute')
+const dbURI = 'mongodb://localhost:27017/AquaSoft'
+const uri = "mongodb+srv://user:parola@cluster0.w3zdp.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+app.use('/employee', EmployeeRoute)
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000...')
