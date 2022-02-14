d8dbcf83191f8251d940e1c06840ea6a3be53e61
Florin-Sabin Sarca <39478000+SabinSJ@users.noreply.github.com>
2022-02-14 12:43:54 +0200

    get employees from backend

--- frontend/src/app/Components/pages/employees/employees.component.ts
+++ frontend/src/app/Components/pages/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  data = [];
+
+  displayedColumns = ['Name','Address','Email','Hire_date','Salary','Job_Title','project_id'];
 
   ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/employee/getAllEmployees').subscribe(data => {
+      this.data = data;
+    })
   }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { EmployeesComponent } from './Components/pages/employees/employees.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
